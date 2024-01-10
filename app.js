78acb48e4a1b1200864c04f1c7ee0d04f23527a1
komanseok <msggun65@gmail.com>
2024-01-10 17:24:32 +0900

    whitelist add

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const whitelist = [];
+const whitelist = ['https://ace.betfun.biz'];
 
 if (process.env.NODE_ENV == 'development') whitelist.push(`http://127.0.0.1:${process.env.PORT}`);
 
